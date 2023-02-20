056cdbfbab8f4a6ceb5f869833d494f10e0a674c
S-Belt <beltadzes@gmail.com>
2023-02-20 12:02:16 +0100

    First

---
+++ src/main/java/GOB/GobApplication.java
@@ -0,0 +1,13 @@
+package GOB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GOB/GobApplicationTests.java
@@ -0,0 +1,13 @@
+package GOB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
